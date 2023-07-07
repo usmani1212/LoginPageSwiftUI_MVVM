bd16fef5e7211b14a77b878125109e689b96e7cc
usmani1212 <usman.m@letsumai.com>
2023-07-07 11:42:28 +0800

    Initial Commit

---
+++ LoginPageUmai_SwiftUI_MVVM/LoginPageUmai_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageUmai_SwiftUI_MVVMApp.swift
+//  LoginPageUmai_SwiftUI_MVVM
+//
+//  Created by Muhammad Usman on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageUmai_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
